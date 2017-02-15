bb48bc99a4dfc221aa6b8b782f614ce529c5ba7e
Amrutha Patel <amruthapatel@gmail.com>
2017-02-15 09:24:45 -0800

    earth

---
+++ src/EarthClass.java
@@ -0,0 +1,12 @@
+
+public class EarthClass {
+
+	public static void main(String[] args) {
+		String [] names = {"MP", "VP", "AP", "XP"};
+		
+		//print greetings here
+		for(int i=0; i<names.length; i++){
+			System.out.println("Hello " + names[i]+"\n");
+	}
+	}
+}
